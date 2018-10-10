9b005550b677cb330cbb5359cf2ce0c06e75f332
Melanie Tosik <melanie.tosik@gmail.com>
2018-10-10 08:30:24 -0400

    ngram plots

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# Bag-of-words (BOW) sentiment classifier
+# Bag-of-words sentiment classifier
+
+[Natural Language Processing with Representation Learning (DS-GA 1011)](https://docs.google.com/document/d/1o0TTWocbkqPa9qsTCXnEFXf3NZzwZLLLSw7SSZmNla8/edit#)
 
 ## Requirements
 
@@ -26,41 +28,46 @@ $ tar -xvzf aclImdb_v1.tar.gz
 $ #rm aclImdb_v1.tar.gz
 ```
 
-## Ablation study
+## Overview
+
+See `main.py` for the main script to run. Change default parameter settings in `settings.py`. Slightly modified versions of the lab code can be found in `bow_model.py` and `torch_data_loader.py`. The `utils.py` script contains all of the preprocessing code. Finally, the plots and result tables are generated in `plot.py` (see also `plots/` and `results/`).
+
+## Preliminary results
+
+### Ablation study
 
 - Tokenzation schemes
 - Number of epochs
 - N-gram size
 - Vocabulary size
 - Embedding size
-- Optimimizer
+- Optimimizer (Adam vs. SGD)
 - Learning rate
-- Dropout
 
-### Tokenization
+#### Tokenization
 
-0. Baseline (`string.split()`)
+0. Baseline: `string.split()`
 1. Tokenization using spaCy
 2. Tokenization using spaCy, filtering of stop words and punctuation
 3. Tokenization using spaCy, filtering of stop words and punctuation, lemmatization
 
-Evidently, the second tokenization scheme [2] works best. Lemmatization seems to be overkill, but filtering stop words and punctuation is helpful. It looks like the model is overfitting though, so let's adjust the learning rate next.
+So far, the second tokenization scheme `[2]` works best. Lemmatization seems to be overkill, but filtering stop words and punctuation is helpful. It looks like the model is overfitting though, so let's adjust the learning rate next.
 
-### Learning rate
+####  Learning rate
 
-The default learning rate was set to 0.01, which is pretty high for the Adam optimizer. Evaluating:
+The default learning rate was set to `0.01`, which is pretty high for the Adam optimizer. Results:
 	
-	- 1e-2: clearly overfitting
-	- 1e-3: pretty good
-	- 1e-4: too slow
+- `1e-2`: clearly overfitting
+- `1e-3`: nice learning curve
+- `1e-4`: too slow
 
-Going to stick with 1e-3 and reduce the number of epochs from 10 to 2.
+We will stick with a learning of `1e-3` for Adam for now. It looks like we can also reduce the number of epochs from 10 to 2 for the following experiments.
 
-### N-gram size
+####  N-gram and vocabulary size
 
-Not conclusive, try increasing vocab size.
+Testing n-gram sizes between `[1-4]` surprisingly did not yield very promising results. Evaluating different vocabulary sizes [`10k, 50k, 100k`] did not significantly affect model performance either. Reverting the tokenization scheme back to `[1]` and _including_ stop words and punctuation improves the results, especially when using bigrams over unigrams. Increasing the number of epochs to 5 again was necessary for the learning curve to converge. Still, the best results are consistently achieved with the more rigorous tokenization scheme `[2]` and using unigrams [`n=1`] only.
+
+#### Embedding size
 
 
-
-
-
+#### Optimizer
--- plot.py
+++ plot.py
@@ -17,6 +17,7 @@ r_dir = settings.RESULTS_DIR
 def tokenization(e=10):
     r = pickle.load(open(r_dir + "tokenization.epochs={}.pkl".format(e), "rb"))
     fig, ax = plt.subplots(1, 1)
+    ax.set_ylim(60, 100)
     lin = np.linspace(0, e, e * 6)
     # Scheme 0
     plt.plot(lin, r[0]["train"], color=CB[0], label="[0] train")
@@ -25,7 +26,7 @@ def tokenization(e=10):
     plt.plot(lin, r[1]["train"], color=CB[1], label="[1] train")
     plt.plot(lin, r[1]["val"], color=CB[1], linestyle="--", label="[1] val")
     # Scheme 2
-    plt.plot(lin, r[2]["train"], color=CB[2], label="[2] train")
+    plt.plot(lin, r[2]["train"] , color=CB[2], label="[2] train")
     plt.plot(lin, r[2]["val"], color=CB[2], linestyle="--", label="[2] val")
     # Scheme 3
     plt.plot(lin, r[3]["train"], color=CB[3], label="[3] train")
@@ -36,11 +37,15 @@ def tokenization(e=10):
     ax.set_xticks(np.arange(0, e + 1, 1))
     ax.legend()
     plt.savefig("plots/tokenization.eps", format="eps", dpi=1000)
+    for key in r:
+        print("scheme=[{}]; best training accuracy: {}; best validation accuracy: {}".format(
+            key, max(r[key]["train"]), max(r[key]["val"])))
 
 
 def adam_lr(e=10):
     r = pickle.load(open(r_dir + "adam_lr.pkl", "rb"))
     fig, ax = plt.subplots(1, 1)
+    ax.set_ylim(60, 100)
     lin = np.linspace(0, e, e * 6)
     # 0.01
     plt.plot(lin, r[1e-2]["train"], color=CB[0], label="[1e-2] train")
@@ -56,12 +61,17 @@ def adam_lr(e=10):
     ax.set_ylabel("train/validation accuracy")
     ax.set_xticks(np.arange(0, e + 1, 1))
     ax.legend()
-    plt.savefig("plots/adam_learning_rate.eps", format="eps", dpi=1000)
+    plt.savefig("plots/adam_lr.eps", format="eps", dpi=1000)
+    for key in r:
+        print("lr=[{}]; best training accuracy: {}; best validation accuracy: {}".format(
+            key, max(r[key]["train"]), max(r[key]["val"])))
 
 
-def ngrams(e=2):
-    r = pickle.load(open(r_dir + "ngrams.pkl", "rb"))
+def ngrams(e=2, scheme=2, vocab_size=100000):
+    r = pickle.load(
+        open(r_dir + "ngrams.scheme={}.vocab={}.pkl".format(scheme, vocab_size), "rb"))
     fig, ax = plt.subplots(1, 1)
+    ax.set_ylim(60, 100)
     lin = np.linspace(0, e, e * 6)
     # Scheme 0
     plt.plot(lin, r[1]["train"], color=CB[0], label="[n<=1] train")
@@ -75,15 +85,78 @@ def ngrams(e=2):
     # Scheme 3
     plt.plot(lin, r[4]["train"], color=CB[3], label="[n<=4] train")
     plt.plot(lin, r[4]["val"], color=CB[3], linestyle="--", label="[n<=4] val")
-    ax.set_title("N-gram size [n<=1-4]")
+    ax.set_title("N-gram size [n<=1-4; vocab={}; scheme={}]".format(vocab_size, scheme))
     ax.set_xlabel("# of epochs")
     ax.set_ylabel("train/validation accuracy")
     ax.set_xticks(np.arange(0, e + 1, 1))
     ax.legend()
-    plt.savefig("plots/ngrams.eps", format="eps", dpi=1000)
+    plt.savefig("plots/ngrams.scheme={}.vocab={}.eps".format(scheme, vocab_size), format="eps", dpi=1000)
+    for key in r:
+        print("ngrams=[{}]; best training accuracy: {}; best validation accuracy: {}".format(
+            key, max(r[key]["train"]), max(r[key]["val"])))
 
 
 if __name__ == "__main__":
     #tokenization()
+    """
+        scheme=[0]; best training accuracy: 99.52; best validation accuracy: 86.0
+        scheme=[1]; best training accuracy: 99.29; best validation accuracy: 85.88
+        scheme=[2]; best training accuracy: 99.61; best validation accuracy: 87.94
+        scheme=[3]; best training accuracy: 99.57; best validation accuracy: 87.42
+    """
+
     #adam_lr()
-    ngrams()
+    """
+        lr=[0.01];  best training accuracy: 99.57;  best validation accuracy: 88.04
+        lr=[0.001]; best training accuracy: 98.04;  best validation accuracy: 87.76
+        lr=[0.0001]; best training accuracy: 85.2;  best validation accuracy: 84.16
+    """
+
+    #ngrams(scheme=2, vocab_size=10000)
+    """
+        ngrams=[1]; best training accuracy: 89.335; best validation accuracy: 87.2
+        ngrams=[2]; best training accuracy: 88.52;  best validation accuracy: 85.6
+        ngrams=[3]; best training accuracy: 87.315; best validation accuracy: 85.24
+        ngrams=[4]; best training accuracy: 86.91;  best validation accuracy: 85.08
+    """
+
+    #ngrams(scheme=2, vocab_size=50000)
+    """
+        ngrams=[1]; best training accuracy: 91.53;  best validation accuracy: 87.88
+        ngrams=[2]; best training accuracy: 90.57;  best validation accuracy: 85.02
+        ngrams=[3]; best training accuracy: 89.925; best validation accuracy: 85.78
+        ngrams=[4]; best training accuracy: 89.41;  best validation accuracy: 86.2
+    """
+
+    #ngrams(scheme=2, vocab_size=100000)
+    """
+        ngrams=[1]; best training accuracy: 91.45;  best validation accuracy: 87.48
+        ngrams=[2]; best training accuracy: 91.785; best validation accuracy: 86.3
+        ngrams=[3]; best training accuracy: 90.72;  best validation accuracy: 85.96
+        ngrams=[4]; best training accuracy: 90.055; best validation accuracy: 86.32
+    """
+
+    #ngrams(scheme=1, vocab_size=10000, e=5)
+    """
+        ngrams=[1]; best training accuracy: 91.455; best validation accuracy: 86.34
+        ngrams=[2]; best training accuracy: 89.205; best validation accuracy: 85.02
+        ngrams=[3]; best training accuracy: 88.85;  best validation accuracy: 85.06
+        ngrams=[4]; best training accuracy: 88.7;   best validation accuracy: 84.34
+    """
+
+    #ngrams(scheme=1, vocab_size=50000, e=5)
+    """
+        ngrams=[1]; best training accuracy: 95.09;  best validation accuracy: 87.44
+        ngrams=[2]; best training accuracy: 94.415; best validation accuracy: 86.62
+        ngrams=[3]; best training accuracy: 93.47;  best validation accuracy: 86.66
+        ngrams=[4]; best training accuracy: 93.35;  best validation accuracy: 85.98
+    """
+
+    #ngrams(scheme=1, vocab_size=100000, e=5)
+    """
+        ngrams=[1]; best training accuracy: 95.855; best validation accuracy: 87.16
+        ngrams=[2]; best training accuracy: 95.88;  best validation accuracy: 86.72
+        ngrams=[3]; best training accuracy: 94.79;  best validation accuracy: 87.28
+        ngrams=[4]; best training accuracy: 94.82;  best validation accuracy: 86.46
+    """
+
